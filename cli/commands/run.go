6a0dcd9a4f0e4e3519f985e6f1c5f62ca3b97845
Jingkai He <jaxihe@gmail.com>
2016-04-02 17:11:28 +0100

    Parse the params like an adult.

--- cli/commands/run.go
+++ cli/commands/run.go
@@ -16,21 +16,25 @@ type Run struct {
 // Help command of the run
 func (r Run) Help() string {
 	helpText := `
-Usage: kode run [options] [filename]
+Usage: kode run [filename] [options]
 
   Auto detect the programming language of the file and run it remoted.
   The result will be displayed onto the terminal asynchronously.
 
-Options:
+filename:
 
-  -v=<version> Version of the programming language you want to use
+	The file that contains the source code you want to run
+
+options:
+
+  -version=<version> Version of the programming language you want to use
 
   -endpoint=<url> The endpoint that you want the code to be run on
 
 Examples:
 
   $ kode run main.go
-  $ kode run -v=2.3.0 foo.rb
+  $ kode run -version=2.3.0 foo.rb
 `
 	return strings.TrimSpace(helpText)
 }
@@ -42,26 +46,31 @@ func (r Run) ShortDescription() string {
 
 // Exec is the command that will execute the Run command
 func (r Run) Exec(args []string) int {
-	var endpoint string
-	var langVersion string
+	// Parse the version and endpoint from the arguments passed in
+	flagargs := args[1:]
 
-	flag.StringVar(&endpoint, "endpoint", "http://127.0.0.1:8080/", "Endpoint of the API")
-	flag.StringVar(&langVersion, "version", "", "Version of the language")
-	flag.Parse()
+	fmt.Println(flagargs)
 
-	runner, err := client.NewRunner(langVersion, args[0])
+	runFlagSet := flag.NewFlagSet("run", flag.ExitOnError)
+	endpoint := runFlagSet.String("endpoint", "http://127.0.0.1:8080/", "Endpoint of the API")
+	langVersion := runFlagSet.String("version", "", "Version of the language")
+
+	runFlagSet.Parse(flagargs)
+
+	// Started running the code
+	runner, err := client.NewRunner(*langVersion, args[0])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		return 1
 	}
 
-	err = runner.FetchUUID(endpoint)
+	err = runner.FetchUUID(*endpoint)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: Failed to fetch UUID - %v\n", err)
 		return 1
 	}
 
-	err = runner.Run(endpoint)
+	err = runner.Run(*endpoint)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: Failed to execute the code - %v\n", err)
 		return 1
